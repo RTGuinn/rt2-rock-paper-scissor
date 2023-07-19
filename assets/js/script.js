7c49cfcf0bc364580bb48492a3f8a44f869caf50
RTGuinn <>
2023-07-19 08:27:26 +0000

    Add compare choices function to script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 function playerChoice() {
 
 }
@@ -6,8 +8,23 @@ function computerChoice() {
 
 }
 
-function compareChoices() {
+/**
+ * This function compares the player choice to the computer random choice
+ * and tells whether you win or lose the game  
+ */
+function compareChoices(playerChoice, computerChoice) {
 
+    if (playerChoice === computerChoice) {
+        return "It's a tie!";
+    } else if (
+        (playerChoice === "rock" && computerChoice === "scissor") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissor" && computerChoice === "paper")
+    ) {
+        return "You Win! Yay!";
+    } else {
+        return "You Lose! Sorry, try again!";
+    }
 }
 
 function incrementWin() {
